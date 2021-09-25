3b0f12c46d65f2d374a231c2e11c299f9e0c98f8
“htian951377408 <951377408@qq.com>
2021-09-25 13:49:24 +0800

    first

---
+++ src/test/java/testcase/LoginTest.java
@@ -0,0 +1,19 @@
+package testcase;
+
+import org.testng.annotations.Test;
+
+/**
+ * @Project: jenkins_test
+ * @Site: http://www.lemonban.com
+ * @Forum: http://testingpai.com
+ * @Copyright: java32期 T0123
+ * @Author: tian
+ * @Create: 2021-09-24 23:43
+ * @Desc：
+ **/
+public class LoginTest {
+    @Test
+    public void LoginSuccess(){
+        System.out.println("登录成功");
+    }
+}
---
+++ src/test/java/testcase/RegisterTest.java
@@ -0,0 +1,19 @@
+package testcase;
+
+import org.testng.annotations.Test;
+
+/**
+ * @Project: jenkins_test
+ * @Site: http://www.lemonban.com
+ * @Forum: http://testingpai.com
+ * @Copyright: java32期 T0123
+ * @Author: tian
+ * @Create: 2021-09-24 23:45
+ * @Desc：
+ **/
+public class RegisterTest {
+    @Test
+    public void registerSuccuess() {
+        System.out.println("注册成功");
+    }
+}
